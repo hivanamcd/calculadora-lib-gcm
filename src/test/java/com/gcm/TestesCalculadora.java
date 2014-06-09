0dcf8247f0eb1ad03f807447722aaf59ac8f31c7
Hivana Macedo <hivanamaced@gmail.com>
2014-06-09 06:57:30 -0300

    close #3 - testes para Calculadora criados

---
+++ src/test/java/com/gcm/TestesCalculadora.java
@@ -0,0 +1,64 @@
+package com.gcm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.gcm.Calculadora;
+
+public class TestesCalculadora {
+
+	private Calculadora c;
+
+	@Before
+	public void setUp() throws Exception {
+		c = new Calculadora();
+	}
+
+	@Test
+	public void testeSoma() {
+		for(int i = -1; i < 2; i++) {
+			for(int j = -1; j < 2; j++) {
+				int actual = i+j;
+				assertEquals(c.soma(i, j), actual);
+			}
+		}
+
+	}
+
+	@Test
+	public void testeDivisao() {
+		for(int i = -1; i < 2; i++) {
+			for(int j = -1; j < 2; j++) {
+				if(j != 0) {
+					int actual = i/j;
+					assertEquals(c.divisao(i, j), actual);
+				} else {
+					assertEquals(c.divisao(i, j), 0);
+				}
+			}
+		}
+	}
+
+	@Test
+	public void testeMultiplicacao() {
+		for(int i = -1; i < 2; i++) {
+			for(int j = -1; j < 2; j++) {
+				int actual = i*j;
+				assertEquals(c.multiplicacao(i, j), actual);
+			}
+		}
+	}
+
+	@Test
+	public void testeSubtraicao() {
+		for(int i = -1; i < 2; i++) {
+			for(int j = -1; j < 2; j++) {
+				int actual = i-j;
+				assertEquals(c.subtracao(i, j), actual);
+			}
+		}
+	}
+
+}
